3657d115454b6e7f2b9ca26325ab7850d79e32b4
mhamdi louay <C:\Users\mhamd\AppData\Roaming\The Bat!>
2024-12-13 04:30:56 +0100

    Activity 3
    Renamed all variables according to the requirements

--- start/cleancode-kata-primeprinter/src/main/java/com/cleancode/knuth/PrimePrinterHelper.java
+++ start/cleancode-kata-primeprinter/src/main/java/com/cleancode/knuth/PrimePrinterHelper.java
@@ -1,13 +1,20 @@
 package com.cleancode.knuth;
 
 public class PrimePrinterHelper {
-    private static final int M = 1000;
-    private static final int RR = 50;
-    private static final int CC = 4;
-    private static final int ORDMAX = 30;
+    private static final int numberOfPrimes = 1000;
+    private static final int linesPerPage = 50;
+    private static final int columns = 4;
+    private static final int ordmax = 30;
 
-    private int[] P = new int[M+1];
-    private int[] MULT = new int[ORDMAX+1];
+    private final int[] primes = new int[numberOfPrimes + 1];
+    private final int[] multiples = new int[ordmax + 1];
+
+    private int candidate = 1;
+    private int primeIndex = 1;
+    private int ord = 2;
+    private int square = 9;
+    private int pageNumber = 1;
+    private int pageOffset = 1;
 
     public void printPrimes() {
         generatePrimes();
@@ -15,55 +22,53 @@ public class PrimePrinterHelper {
     }
 
     private void generatePrimes() {
-        int J = 1;
-        int K = 1;
-        P[1] = 2;
-        int ORD = 2;
-        int SQUARE = 9;
+        primes[1] = 2;
 
-        while (K < M) {
-            boolean JPRIME;
+        while (primeIndex < numberOfPrimes) {
+            boolean possiblyPrime;
             do {
-                J += 2;
-                if (J == SQUARE) {
-                    ORD++;
-                    SQUARE = P[ORD] * P[ORD];
-                    MULT[ORD-1] = J;
+                candidate += 2;
+                if (candidate == square) {
+                    ord++;
+                    square = primes[ord] * primes[ord];
+                    multiples[ord - 1] = candidate;
                 }
-                int N = 2;
-                JPRIME = true;
-                while (N < ORD && JPRIME) {
-                    while (MULT[N] < J)
-                        MULT[N] += P[N] + P[N];
-                    if (MULT[N] == J)
-                        JPRIME = false;
-                    N++;
+                int n = 2;
+                possiblyPrime = true;
+                while (n < ord && possiblyPrime) {
+                    while (multiples[n] < candidate) {
+                        multiples[n] += primes[n] + primes[n];
+                    }
+                    if (multiples[n] == candidate) {
+                        possiblyPrime = false;
+                    }
+                    n++;
                 }
-            } while (!JPRIME);
-            K++;
-            P[K] = J;
+            } while (!possiblyPrime);
+            primeIndex++;
+            primes[primeIndex] = candidate;
         }
     }
 
     private void printPrimeNumbers() {
-        int PAGENUMBER = 1;
-        int PAGEOFFSET = 1;
-        while (PAGEOFFSET <= M) {
+        while (pageOffset <= numberOfPrimes) {
             System.out.print("The First ");
-            System.out.print(Integer.toString(M));
+            System.out.print(Integer.toString(numberOfPrimes));
             System.out.print(" Prime Numbers === Page ");
-            System.out.print(Integer.toString(PAGENUMBER));
+            System.out.print(Integer.toString(pageNumber));
             System.out.println("\n");
 
-            for (int ROWOFFSET = PAGEOFFSET; ROWOFFSET <= PAGEOFFSET + RR - 1; ROWOFFSET++) {
-                for (int C = 0; C <= CC - 1; C++)
-                    if (ROWOFFSET + C * RR <= M)
-                        System.out.printf("%10d", P[ROWOFFSET + C * RR]);
+            for (int rowOffset = pageOffset; rowOffset <= pageOffset + linesPerPage - 1; rowOffset++) {
+                for (int column = 0; column <= columns - 1; column++) {
+                    if (rowOffset + column * linesPerPage <= numberOfPrimes) {
+                        System.out.printf("%10d", primes[rowOffset + column * linesPerPage]);
+                    }
+                }
                 System.out.println();
             }
             System.out.println("\f");
-            PAGENUMBER++;
-            PAGEOFFSET += RR * CC;
+            pageNumber++;
+            pageOffset += linesPerPage * columns;
         }
     }
 }
